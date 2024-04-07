71c4553f66a130cae87ad6b1d697c777f8e55e08
sertschgi <seppi@neureiter.bayern>
2024-04-07 13:44:43 +0200

    pre-5

--- install_deps.sh
+++ install_deps.sh
@@ -3,4 +3,7 @@ python3 -m pip install tflite-runtime
 sudo apt install python3-opencv
 sudo apt install apt-transport-https curl gnupg -y
 sudo apt-get update
-sudo apt-get install python3-pycoral
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install libedgetpu1-std
