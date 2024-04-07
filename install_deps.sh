1829543863748f6ad0c474f1013f5cba04667bf3
sertschgi <seppi@neureiter.bayern>
2024-04-07 14:12:06 +0200

    pre-7

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,3 @@
-python3 -m pip install tensorflow
-python3 -m pip install tflite-runtime
 sudo apt install python3-opencv
 sudo apt install apt-transport-https curl gnupg -y
 sudo apt-get update
@@ -7,3 +5,13 @@ echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sud
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install libedgetpu1-std
+
+python3 -m venv .airpi --system-site-packages
+
+touch activate.sh
+echo "source .airpi/bin/activate" > activate.sh
+sudo chmod +x activate.sh
+
+source activate.sh
+python3 -m pip install tensorflow
+python3 -m pip install tflite-runtime
